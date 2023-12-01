044ef810e03292201ec0060e3e6ce5b21ac0a20c
Richard Russell <richard.russell@hashicorp.com>
2023-12-01 18:05:38 +0000

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,14 +3,14 @@ terraform {
   required_version = ">= 1.6.0"
   # Backend on Terraform Cloud or Terraform Enterprise
   # comment the cloud{} block to work with local state.
-  cloud {
-    organization = "richard-russell-org"
-    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io. Update with TFE hostname if required
-    workspaces {
-      name    = "aws-terraform-demo-ec2-nlb" //update me
-      project = "terraform-modules"
-    }
-  }
+  # cloud {
+  #   organization = "richard-russell-org"
+  #   hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io. Update with TFE hostname if required
+  #   workspaces {
+  #     name    = "aws-terraform-demo-ec2-nlb" //update me
+  #     project = "terraform-modules"
+  #   }
+  # }
 }
 
 provider "aws" {
